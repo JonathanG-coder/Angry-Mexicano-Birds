8c895ea2f0d420c0b4bddca982a54fd8d6ee1624
jonathan <jonathangager75@gmail.com>
2024-07-18 13:48:09 +0200

    Travail finis

--- Assets/Player_controler.cs
+++ Assets/Player_controler.cs
@@ -1,83 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player_controler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField]private GameObject ballPrefab;
-
-    [SerializeField]private GameObject pivot;
-
-    [SerializeField]private GameObject currentPrefab;
-    [SerializeField] float detachDelayTime = 0.25F ;
-
-
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private GameObject pivot;
+    [SerializeField] float detachDelayTime = 0.5f;
+    [SerializeField] float spawnDelayTime = 3;
+    private GameObject currentBall;
     private Camera cam;
     private Rigidbody2D ballRigidBody;
     private SpringJoint2D ballSpringJoint;
     private bool isDragging = false;
+    private Vector2 initialBallPosition;
 
-    private Vector2 initialBalPosition;
-
-
+    // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-        SpawnBall
-
-        ballRigidBody = currentBall.GetComponent<Rigidbody2D>();
-        initialBalPosition = currentBallRigidBody.position;
-        ballSpringJoint = currentBall.GetComponent<SpringJoint2D>();  
-
-
-        //manque kkl chose 
-        //Touchscreen.current.primaryTouch.press.IsPressed();
-        //Touchscreen.current.primaryTouch.position.ReadValue();
+        SpawnBall();
+        initialBallPosition = ballRigidBody.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.IsPressed())
+        if(!Touchscreen.current.primaryTouch.press.IsPressed())
         {
-            Debug.Log("Je touche pas");
-            ballRigidBody.isKinematic = false;
-            if(isDragging) 
+            if(isDragging && ballRigidBody != null)
             {
+                ballRigidBody.isKinematic = false;
                 Invoke(nameof(DetachBall), detachDelayTime);
                 isDragging = false;
-                Invoke(nameof(RestartBall), 0.3F);
+                Invoke(nameof(SpawnBall), spawnDelayTime);
+                // Invoke(nameof(RestartBall), 3);
             }
-
             return;
         }
 
         isDragging = true;
-        ballRigidBody.isKinematic = true;
-        Vector2 worldPosition = cam.ScreenToWorldPoint
-            (Touchscreen.current.primaryTouch.position.ReadValue());
-        ballRigidBody.position = worldPosition;
-
+        if(ballRigidBody != null)
+        {
+            ballRigidBody.isKinematic = true;
+            Vector2 worldPosition = cam.
+                ScreenToWorldPoint(Touchscreen.current.primaryTouch.position.ReadValue());
+            ballRigidBody.position = worldPosition;
+        }
     }
 
     private void DetachBall()
     {
         ballSpringJoint.enabled = false;
+        ballRigidBody = null;
     }
 
-    private void RestartBall()
-    {
-        ballRigidBody.position = initialBalPosition;
-        ballRigidBody.velocity = new Vector2(0,0);
-        ballSpringJoint.enabled = true ;
-    }
+    // private void RestartBall()
+    // {
+    //     ballRigidBody.position = initialBallPosition;
+    //     ballRigidBody.velocity = new Vector2(0,0);
+    //     ballSpringJoint.enabled = true;
+    // }
 
     private void SpawnBall()
     {
-        currentBall = Instantiate(ballPrefab, pivot.transform);
-        ballSpringJoint.connectedBody= pivot.GetComponent<Rigidbody2D>();
+        if(currentBall != null)
+        {
+            Destroy(currentBall);
+        }
+
+        currentBall = Instantiate(ballPrefab);
+        currentBall.transform.position = pivot.transform.position;
+        ballRigidBody = currentBall.GetComponent<Rigidbody2D>();
+        ballSpringJoint = currentBall.GetComponent<SpringJoint2D>();
+        ballSpringJoint.connectedBody = pivot.GetComponent<Rigidbody2D>();
     }
 }
--- Assets/TouchInput.cs
+++ Assets/TouchInput.cs
@@ -12,50 +12,12 @@ public class TouchInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main; // OT cette phrase ?
-
-        ballRigidBody = ball.GetComponent<Rigidbody2D>(); 
-        //Touchscreen.current.primaryTouch.press.IsPressed();
-        //Touchscreen.current.primaryTouch.position.ReadValue();
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        ballRigidBody = ball.GetComponent<Rigidbody2D>();
 
-
-
-        if (!Touchscreen.current.primaryTouch.press.IsPressed())
-        {
-
-            Vector2 worldPosition = cam.ScreenToWorldPoint
-            (Touchscreen.current.primaryTouch.position.ReadValue());
-
-
-            ballRigidBody.isKinematic = true;
-            ballRigidBody.position = worldPosition;
-            
-        }
-
-        if(ballRigidBody != null)
-        {
-            ballRigidBody.isKinematic = false;
-        }
-        
-        
-
-
-        // // Out the method if not touching the screen
-        // if (!Touchscreen.current.primaryTouch.press.IsPressed())
-        // {
-        //     return;
-        // }
-
-        // Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-        // Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-
-        // Debug.Log("TOUCH POSITION: " + touchPosition);
-        // Debug.Log("WORLD POSITION: " + worldPosition);
     }
 }
