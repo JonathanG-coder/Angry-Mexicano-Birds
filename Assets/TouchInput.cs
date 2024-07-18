22a73a7dd71644ddc8df4e3766fd0ff53afa9dd7
jonathan <jonathangager75@gmail.com>
2024-07-18 10:25:29 +0200

    start projet avec touch input

--- Assets/TouchInput.cs
+++ Assets/TouchInput.cs
@@ -5,27 +5,33 @@ using UnityEngine.InputSystem;
 
 public class TouchInput : MonoBehaviour
 {
-    private Camera mainCamera;
+    private Camera Camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = Camera.main;
+        Camera = Camera.main; // OT cette phrase ? 
+        //Touchscreen.current.primaryTouch.press.IsPressed();
+        //Touchscreen.current.primaryTouch.position.ReadValue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Out the method if not touching the screen
-        if (!Touchscreen.current.primaryTouch.press.IsPressed())
-        {
-            return;
-        }
+        Debug.Log(Touchscreen.current.primaryTouch.position.ReadValue());
+        Vector2 worldPosition = Camera.ScreenToWorldPoint(Touchscreen.current.primaryTouch.position.ReadValue());
 
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log("TOUCH POSITION: " + touchPosition);
-        Debug.Log("WORLD POSITION: " + worldPosition);
+        // // Out the method if not touching the screen
+        // if (!Touchscreen.current.primaryTouch.press.IsPressed())
+        // {
+        //     return;
+        // }
+
+        // Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        // Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        // Debug.Log("TOUCH POSITION: " + touchPosition);
+        // Debug.Log("WORLD POSITION: " + worldPosition);
     }
 }
